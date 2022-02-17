0c3b274655a531650eb6cf4bb1f996b3a28ad64a
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-02-17 17:18:07 -0300

    Criando CRUD MVC

---
+++ ManagerUser/Models/Employee.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManagerUser.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; } 
+        
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "Preenchimento obrigatório")]
+        [DisplayName("Nome")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Matricula")]
+        public int Code { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Cargo")]
+        public int Position { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Localização")]
+        public int OfficeLocation { get; set; }
+
+        public Employee(int id, string name, int code, int position, int officeLocation)
+        {
+            Id = id;
+            Name = name;
+            Code = code;
+            Position = position;
+            OfficeLocation = officeLocation;
+        }
+
+    }
+}
---
+++ ManagerUser/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManagerUser.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+    }
+}
